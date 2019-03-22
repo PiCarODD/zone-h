b78e0bb666124e6c839dffeba6d8257019602f82
picaro <heinjame@gmail.com>
2019-03-22 17:34:10 +0630

    Update zoneh.py

--- zoneh.py
+++ zoneh.py
@@ -1,6 +1,6 @@
 import requests
 from huepy import *
-form time import *
+from time import *
 print (green(""" _____                       _     
 / _  / ___  _ __   ___      | |__  
 \// / / _ \| '_ \ / _ \_____| '_ \ 
@@ -10,19 +10,29 @@ print (green(""" _____                       _
 """))
 attacktype=raw_input("Choose 1 for Single 2 for Mass : ")
 def single(attacker,web):
+	hack=web.split('/')
+	reason=1
+	if 'index.php' in hack or 'index.html' in hack or len(hack)<4 or hack[len(hack)-1]=='':
+		reason=3
 	url="http://zone-h.com/notify/single"
-	payload={'defacer':attacker,'domain1':web,'hackmode':1,'reason':1}
+	payload={'defacer':attacker,'domain1':web,'hackmode':1,'reason':reason}
 	r=requests.post(url,data=payload)
 	if 'OK' in r.text:
 		print (good("Done Posting !"))
 	else:
 		print(bad("Invalid Domain or domain has been defaced !"))
+	print (info("Enjoy my tool and give me stars fuck you!"))
 def mass(attacker,lists):
 	with open(lists,'r') as f:
 		f=f.readlines()
 		url="http://zone-h.com/notify/single"
 		for i in range(len(f)):
-			payload={'defacer':attacker,'domain1':f[i].strip('\n'),'hackmode':1,'reason':1}
+			web=f[i].strip('\n')
+			hack=web.split('/')
+			reason=1
+			if 'index.php' in hack or 'index.html' in hack or len(hack)<4 or hack[len(hack)-1]=='':
+				reason=3
+			payload={'defacer':attacker,'domain1':f[i].strip('\n'),'hackmode':1,'reason':reason}
 			r=requests.post(url,data=payload,headers={"Referer": "http://www.zone-h.org/"})
 			if 'OK' in r.text:
 				print (good("Done Posting ! "+f[i].strip('\n')))
@@ -30,7 +40,6 @@ def mass(attacker,lists):
 				print(bad("Invalid Domain or domain has been defaced ! "+f[i].strip('\n')))
 		sleep(3)
 		print "......."
-		sleep(3)
 		print (info("Enjoy my tool and give me stars fuck you!"))
 if attacktype=="1":
 	name=raw_input("Attacker Name : ")
